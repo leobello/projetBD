3f52980cdf6cde68ca9a0e9927349071397160e7
lecunm <martial.lecun@free.fr>
2019-01-30 11:49:19 +0100

    Ajout incrStock dans GestionStock.java

--- src/serviceBD/GestionStock.java
+++ src/serviceBD/GestionStock.java
@@ -6,21 +6,26 @@ import Controler._GlobalControler;
 
 public class GestionStock {
 	private StockControler stkc;
-	
-	public GestionStock(){
+
+	public GestionStock() {
 		stkc = _GlobalControler.getStockControler();
 	}
 
-	public boolean decrStockTirage(int i,String typeImp, String qualite, String format){
+	public boolean decrStock(int i, String typeImp, String qualite, String format) {
 		Stock currentStock = stkc.readStock(typeImp, qualite, format);
-		if (currentStock.getQuantiteStock() < i){
+		if (currentStock.getQuantiteStock() < i) {
 			return false;
-		}else {
-			currentStock.setQuantiteStock(currentStock.getQuantiteStock()-i);
+		} else {
+			currentStock.setQuantiteStock(currentStock.getQuantiteStock() - i);
 			stkc.update(currentStock);
 			return true;
 		}
 
-		
+	}
+
+	public void incrStock(int i, String typeImp, String qualite, String format) {
+		Stock currentStock = stkc.readStock(typeImp, qualite, format);
+		currentStock.setQuantiteStock(currentStock.getQuantiteStock() + i);
+		stkc.update(currentStock);
 	}
 }
